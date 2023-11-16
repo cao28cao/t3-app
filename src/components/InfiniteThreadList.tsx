96102aa83babdaaa3a2e0ff5b5ca5292bb266130
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-11-17 04:12:07 +0700

    the last

--- src/components/InfiniteThreadList.tsx
+++ src/components/InfiniteThreadList.tsx
@@ -68,6 +68,7 @@ export default function InfiniteThreadList({
 
 const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: "short",
+  timeStyle: "short",
 });
 
 function ThreadCard({
