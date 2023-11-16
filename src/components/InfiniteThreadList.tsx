430884ff61ddc89a15c73732fcc06a3d0053a1c1
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-11-17 04:46:53 +0700

    the last

--- src/components/InfiniteThreadList.tsx
+++ src/components/InfiniteThreadList.tsx
@@ -11,7 +11,7 @@ import { LoadingSpinner } from "./LoadingSpinner";
 type Thread = {
   id: string;
   content: string;
-  createdAt: string;
+  createdAt: Date;
   likeCount: number;
   likedByMe: boolean;
   user: {
